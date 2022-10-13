aca8e8184b5d533b13f66e2d841e9e07adfd2c94
Mate <pejnovac.mate@gmail.com>
2022-10-13 09:02:39 +0200

    finaL

--- features/step_definitions/customSteps.js
+++ features/step_definitions/customSteps.js
@@ -9,7 +9,7 @@ const sleep = async (milliseconds) => {
     });
 };
 
-setDefaultTimeout(60*1000)
+setDefaultTimeout(90*1000)
 
 let driver;
 let sum = 0;
@@ -48,25 +48,25 @@ When('I managing my outlook with this bot', async function () {
     await driver.findElement(By.css('input[type="submit"]')).click();
     await sleep(2000);
     await driver.findElement(By.css('input[id="idBtn_Back"]')).click();
-    await sleep(4000);
+    await sleep(8000);
     await driver.findElement(By.css('span[id="id__9"]')).click();
-    await sleep(7000);
+    await sleep(10000);
     await driver.findElement(By.css('div[aria-label="Címzett"]')).sendKeys('nemletezoemailvagyok@gmail.com');
-    await sleep(1000);
+    await sleep(2000);
     await driver.findElement(By.css('input[aria-label="Adja meg a tárgyat"]')).sendKeys('random subject');
     await sleep(1000);
     await driver.findElement(By.css('div[aria-label="Üzenet szövege, nyomja meg az Alt+F10 gombot a kilépéshez"]')).sendKeys('valami szöveg');
-    await sleep(1000);
+    await sleep(2000);
     await driver.findElement(By.css('i[data-icon-name="send"]')).click();
-    await sleep(2000);
+    await sleep(12000);
     await driver.findElement(By.css('i[data-icon-name="GlobalNavButton"]')).click();
-    await sleep(2000);
+    await sleep(4000);
     await driver.findElement(By.css('div[title="Elküldött elemek"]')).click();
-    await sleep(2000);
+    await sleep(4000);
     await driver.findElement(By.css('i[data-icon-name="StatusCircleCheckmark"]')).click();
-    await sleep(2000);
+    await sleep(4000);
     await driver.findElement(By.css('button[name="Mappa ürítése"]')).click();
-    await sleep(2000);
+    await sleep(4000);
 
 
 
